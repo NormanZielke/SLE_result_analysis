0728d24df22e225b7269e1d5c0e2ab3dbe4b4ab4
Norman Zielke <norman.zielke@rl-institut.de>
2025-05-21 12:04:03 +0200

    Check Seperator of csv

--- calc_results.py
+++ calc_results.py
@@ -22,25 +22,26 @@ def dispatch_elec(region):
     return df_dispatch_elec
 
 
-def plot_dir(self):
-    return os.path.join("plots", self.region_id)
-
-
 def cap_opt_bar(region):
 
     df_cap_opt = capacities_opt(region)
+    # Direction for bar-plot
     region_id = region.region_id
     filename = os.path.join("plots", region_id, f"capacities_opt_{region_id}.png")
+
     barplot_c(df_cap_opt,
               title = f"Optimierte Kapazitäten {region.region_id}",
               filename = filename)
+
 
 def dispatch_bar(region):
 
     df_dispatch_elec = dispatch_elec(region)
     df_dispatch_elec.var_value *= 1e-3
+    # Direction for bar-plot
     region_id = region.region_id
     filename = os.path.join("plots", region_id, f"dispatch_elec_{region_id}.png")
+
     barplot_e(df_dispatch_elec,
               title= f"Stromversorgung je Technologie {region.region_id}",
               filename = filename
--- class_region.py
+++ class_region.py
@@ -4,14 +4,26 @@ from calc_results import(
     capacities_opt,
     cap_opt_bar,
     dispatch_bar,
-    plot_dir,
+
 )
 
+def read_csv_auto_sep(path):
+
+    with open(path, 'r') as f:
+        first_line = f.readline()
+        if ";" in first_line:
+            sep = ";"
+        elif "," in first_line:
+            sep = ","
+        else:
+            raise ValueError("Unbekannter CSV-Separator.")
+
+    return pd.read_csv(path, sep=sep)
 
 class region:
     def __init__(self,args, csv_folder=None, region_id=None):
         self.region_id = region_id
-        df_scalars = pd.read_csv(csv_folder, sep=";")
+        df_scalars = read_csv_auto_sep(csv_folder)
         if region_id:
             df_scalars["name"] = df_scalars["name"].str.replace(f"{region_id}-", "", regex=False)
         self.scalars = df_scalars
